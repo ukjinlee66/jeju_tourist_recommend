aa9e2b56246153d88c580fa77071d91d04e277a4
dldydrhkd <sksda4614@naver.com>
2022-03-15 03:36:09 +0900

    프로젝트 생성
    gradle로 build, react 연동 완료

---
+++ jeju_recommand/src/test/java/com/ilda/jeju_recommand/JejuRecommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilda.jeju_recommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuRecommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
