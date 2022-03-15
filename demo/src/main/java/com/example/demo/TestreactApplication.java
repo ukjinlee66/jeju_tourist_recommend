d4ed598b88cfd8fc207d05628a4d8ff88e6b14a1
Youkjin Lee <ukjinlee66@gmail.com>
2022-03-15 17:38:50 +0900

    react env update

---
+++ demo/src/main/java/com/example/demo/TestreactApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestreactApplication.class, args);
+	}
+
+}
