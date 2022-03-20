d735dbb47cab7ab85820e00efec6c07c86a30041
dldydrhkd <sksda4614@naver.com>
2022-03-20 19:57:09 +0900

    TouristAttractionServiceImpl
    관광지 서비스 구현

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/TouristAttaractionServiceImpl.java
@@ -0,0 +1,20 @@
+package kr.pe.playdata.service.impl;
+
+import kr.pe.playdata.domain.visitJeju;
+import kr.pe.playdata.repository.TouristAttractionRepo;
+import kr.pe.playdata.service.TouristAttractionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TouristAttaractionServiceImpl implements TouristAttractionService {
+
+    @Autowired
+    private TouristAttractionRepo touristRepo;
+
+    public List<visitJeju> find(){
+        return touristRepo.findAll();
+    }
+}
