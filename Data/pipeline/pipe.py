f4492d9187d6f7f92877e137a19dcdf3ff662a12
Youkjin Lee <ukjinlee66@gmail.com>
2022-03-22 18:04:19 +0900

    nnp.csv update

--- Data/pipeline/pipe.py
+++ Data/pipeline/pipe.py
@@ -1,8 +1,31 @@
 import pandas as pd
 import json
+import spark
 from pandas import json_normalize
+from json2parquet import convert_json
+import os
 
-with open("/Users/youlee/Desktop/jeju_tourist_recommand/Data/twitter/twitter_crawling.json","r",encoding="utf-8") as f:
-    ddata = json.load(f)
-df = pd.DataFrame(ddata)
-print(df)
+path_dir = "/Users/youlee/Desktop/jeju_tourist_recommand/Data/instagram"
+file_list = os.listdir(path_dir)
+print(file_list)
+for i in file_list:
+    new_path = os.listdir(path_dir+"/"+i)
+    print(new_path)
+    for j in new_path:
+        file_name = path_dir+'/'+i+"/"+j
+        with open(file_name,"r",encoding="utf-8") as f:
+            ddata = json.load(f)
+        df = pd.DataFrame(ddata)
+        df.to_parquet(parquetdir + j+".parquet", compression='gzip')
+
+
+
+# parquetdir = "/Users/youlee/Desktop/jeju_tourist_recommand/Data/parquet_test/
+# datadir = "/Users/youlee/Desktop/jeju_tourist_recommand/Data/"
+
+
+
+# with open(datadir + "twitter/twitter_crawling.json", "r",encoding="utf-8") as f:
+#     ddata = json.load(f)
+# df = pd.DataFrame(ddata)
+# df.to_parquet(parquetdir + "twitteroriginData.parquet", compression='gzip')
