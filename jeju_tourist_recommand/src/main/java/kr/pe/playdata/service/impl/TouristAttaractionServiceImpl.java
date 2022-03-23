a16eb8bf2ae275a300de533213fd5262102e4aea
dldydrhkd <sksda4614@naver.com>
2022-03-23 22:50:17 +0900

    Update TouristAttaractionServiceImpl.java
    findById() optional 사용
    findByTourLike() 구현

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/TouristAttaractionServiceImpl.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/TouristAttaractionServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TouristAttaractionServiceImpl implements TouristAttractionService {
@@ -16,5 +17,14 @@ public class TouristAttaractionServiceImpl implements TouristAttractionService {
 
     public List<visitJeju> findAll(){
         return touristRepo.findAll();
+    }
+
+    public Optional<visitJeju> findById(String id){
+        Optional<visitJeju> jeju = touristRepo.findById(id);
+        return jeju;
+    }
+
+    public List<visitJeju> findByTourLike(String tour){
+        return touristRepo.findByTourLike(tour);
     }
 }
