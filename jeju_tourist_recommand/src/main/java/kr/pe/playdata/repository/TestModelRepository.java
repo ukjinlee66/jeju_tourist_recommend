1950b9fb5dd52ccfd54252c841ba59e0f8c95f57
dldydrhkd <sksda4614@naver.com>
2022-03-23 22:49:02 +0900

    Update TestModelRepository.java
    TestModel

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/TestModelRepository.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/TestModelRepository.java
@@ -1,6 +1,7 @@
 package kr.pe.playdata.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,8 @@ import kr.pe.playdata.domain.testModel;
 
 
 @Repository
-public interface TestModelRepository extends MongoRepository<testModel, String> {
+public interface TestModelRepository extends MongoRepository<testModel, String>{
 	public List<testModel> findAll();
+	public Optional<testModel> findByColumn1(String s);
+	public List<testModel> findByColumn1Like(String s);
 }
