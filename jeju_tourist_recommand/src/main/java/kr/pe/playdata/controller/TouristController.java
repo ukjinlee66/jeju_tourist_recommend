9f01e7ca90e6749bbc255e63119a54afa6b95498
dldydrhkd <sksda4614@naver.com>
2022-03-23 22:48:54 +0900

    TouristController
    findAll() 모든 리스트 반환
    lookUp() id로 찾은 tour 반환

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TouristController.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TouristController.java
@@ -1,5 +1,34 @@
 package kr.pe.playdata.controller;
 
+import kr.pe.playdata.domain.testModel;
+import kr.pe.playdata.domain.visitJeju;
+import kr.pe.playdata.service.TestService;
+import kr.pe.playdata.service.TouristAttractionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tour")
 public class TouristController {
-    
+    @Autowired
+    private TouristAttractionService service;
+
+    @GetMapping("/findAll")
+    public List<visitJeju> findAll(){
+        return service.findAll();
+    }
+//    @GetMapping()
+//    public List<visitJeju> search(){
+//
+//    }
+    @GetMapping("/lookup")
+    public Optional<visitJeju> lookUp(@RequestParam String id){
+        return service.findById(id);
+    }
 }
