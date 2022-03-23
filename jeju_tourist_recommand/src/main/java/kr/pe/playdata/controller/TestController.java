3498cf35d7582952400931011b669207a37a46c2
dldydrhkd <sksda4614@naver.com>
2022-03-23 22:48:04 +0900

    TestController

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TestController.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TestController.java
@@ -1,14 +1,17 @@
 package kr.pe.playdata.controller;
 
+import kr.pe.playdata.domain.visitJeju;
 import kr.pe.playdata.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.pe.playdata.domain.testModel;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/test")
@@ -22,5 +25,15 @@ public class TestController {
 		System.out.println("here is controller");
 		List<testModel> li = service.findAll();
 		return li;
+	}
+
+	@GetMapping("/lookup")
+	public Optional<testModel> lookUp(@RequestParam String column1) {
+		return service.findByColumn1(column1);
+	}
+
+	@GetMapping("/search")
+	public List<testModel> search(@RequestParam String column1) {
+		return service.findByColumn1Like(column1);
 	}
 }
