0073ed2f54c19da451a04c7a3e0c7444e72a3b43
dldydrhkd <sksda4614@naver.com>
2022-03-29 02:47:08 +0900

    Create InstaKeywordMongoRepo.java
    Mongo column 특정 컬럼으로만 가져오도록 만듬

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/InstaKeywordMongoRepo.java
@@ -0,0 +1,20 @@
+package kr.pe.playdata.repository;
+
+import kr.pe.playdata.domain.InstaRank;
+import kr.pe.playdata.domain.SearchRank;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InstaKeywordMongoRepo extends MongoRepository<InstaRank, String> {
+    /*
+        인스타 키워드에 대한 레포지토리
+     */
+
+    @Query(sort = "{cnt : -1}", fields = "{keyword : 1}")
+    public List<InstaRank> findBy(Pageable pageable);       // cnt 많은 기준으로 top n 개를 가져온다.
+}
