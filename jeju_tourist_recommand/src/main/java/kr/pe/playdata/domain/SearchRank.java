fe78282e751faa93a4c6ebdf24e00f4ff4da5c02
dldydrhkd <sksda4614@naver.com>
2022-03-30 09:22:17 +0900

    Update SearchRank.java
    SearchRank collection 설정

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/domain/SearchRank.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/domain/SearchRank.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "keyword")
 public class SearchRank {
 	/*
 	 * 		검색어 순위
