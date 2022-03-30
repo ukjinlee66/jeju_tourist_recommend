4ff5f09ad334be4c20a46bd50e1e5f151d9b4c07
dldydrhkd <sksda4614@naver.com>
2022-03-30 09:24:55 +0900

    class명 변경

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/TouristAttractionTemplateRepo.java
@@ -0,0 +1,9 @@
+package kr.pe.playdata.repository;
+
+import kr.pe.playdata.domain.VisitJejuRandomImg;
+
+import java.util.List;
+
+public interface TouristAttractionTemplateRepo {
+    public List<VisitJejuRandomImg> random();
+}
