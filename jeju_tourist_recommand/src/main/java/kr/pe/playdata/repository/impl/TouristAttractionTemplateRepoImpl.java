c4e759dcd095deed3c6d9325eaf92d1a9d347bde
dldydrhkd <sksda4614@naver.com>
2022-03-30 09:24:17 +0900

    class 명 변경

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/impl/TouristAttractionTemplateRepoImpl.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/repository/impl/TouristAttractionTemplateRepoImpl.java
@@ -1,30 +1,28 @@
 package kr.pe.playdata.repository.impl;
 
-import kr.pe.playdata.domain.visitJejuRandomImg;
-import kr.pe.playdata.repository.TouristAttractionRepo;
+import kr.pe.playdata.domain.VisitJejuRandomImg;
+import kr.pe.playdata.repository.TouristAttractionTemplateRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.SampleOperation;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class TouristAttractionTemplateRepoImpl implements TouristAttractionRepo {
+public class TouristAttractionTemplateRepoImpl implements TouristAttractionTemplateRepo {
 
     @Autowired
     private MongoTemplate mongoTemplate;
 
     @Override
-    public List<visitJejuRandomImg> random() {
+    public List<VisitJejuRandomImg> random() {
         SampleOperation matchStaget = Aggregation.sample(5);
         Aggregation aggregation = Aggregation.newAggregation(matchStaget);
-        AggregationResults<visitJejuRandomImg> output = mongoTemplate.aggregate(aggregation, "visitJeju", visitJejuRandomImg.class);
-        List<visitJejuRandomImg> li = output.getMappedResults();
+        AggregationResults<VisitJejuRandomImg> output = mongoTemplate.aggregate(aggregation, "visitJeju", VisitJejuRandomImg.class);
+        List<VisitJejuRandomImg> li = output.getMappedResults();
         return li;
     }
 }
