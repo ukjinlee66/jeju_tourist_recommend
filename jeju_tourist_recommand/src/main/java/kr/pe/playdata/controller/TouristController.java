50afe4873423a05dfced62cddaab9ad9bf0218ed
dldydrhkd <sksda4614@naver.com>
2022-03-30 09:20:47 +0900

    Update TouristController.java
    이름 변경

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TouristController.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/TouristController.java
@@ -1,8 +1,8 @@
 package kr.pe.playdata.controller;
 
-import kr.pe.playdata.domain.visitJeju;
-import kr.pe.playdata.domain.visitJejuList;
-import kr.pe.playdata.domain.visitJejuRandomImg;
+import kr.pe.playdata.domain.VisitJeju;
+import kr.pe.playdata.domain.VisitJejuList;
+import kr.pe.playdata.domain.VisitJejuRandomImg;
 import kr.pe.playdata.service.TouristAttractionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,22 +24,22 @@ public class TouristController {
     private TouristAttractionService service;
 
     @GetMapping("/lookup")          // id로 관광지 검색
-    public Optional<visitJeju> lookUp(@RequestParam String id){
+    public Optional<VisitJeju> lookUp(@RequestParam String id){
         return service.findById(id);    // null이 반환되는 경우 에러가 나지 않게 Optional을 사용
     }
 
      @GetMapping("/searchByTourName")    // 요청한 이름과 관련된 관광지 검색
-        public List<visitJejuList> searchByTourName(@RequestParam String tour){
+        public List<VisitJejuList> searchByTourName(@RequestParam String tour){
             return service.findByTourLike(tour);   //
     }
 
     @GetMapping("/searchByCertainColumn")       // tour, sub_titile, tag_prev, tag_next, purpose,  etc_property, content 기준으로 검색
-    public List<visitJejuList> searchByCertainColumn(@RequestParam String search){
+    public List<VisitJejuList> searchByCertainColumn(@RequestParam String search){
         return service.search(search);
     }
 
     @GetMapping("/random")              // id로 관광지 검색
-    public List<visitJejuRandomImg> random(){
+    public List<VisitJejuRandomImg> random(){
         return service.randomTour();
     }
 }
