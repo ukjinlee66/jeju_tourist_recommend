76687d58b0af443194136abb7a0166643a309954
dldydrhkd <sksda4614@naver.com>
2022-03-31 17:25:57 +0900

    Update Naver.java
    List<Blog> 에서 Blog로 변경

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/domain/Naver.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/domain/Naver.java
@@ -1,9 +1,13 @@
 package kr.pe.playdata.domain;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Data
+@Document(collection ="naver")      // 없어도 된다.
 public class Naver {
     /*
         관광지 별로 네이버 블로그들을 담는 VO
@@ -12,5 +16,5 @@ public class Naver {
     @Id
     private String id;
     private String source;
-    private List<Blog> contents;
+    private Blog contents;
 }
