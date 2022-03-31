d0afca6296b0ab60070eb5edd0c1ca625d0d9a7a
dldydrhkd <sksda4614@naver.com>
2022-03-31 17:27:06 +0900

    Update BlogServiceImpl.java
    BlogServiceImpl 구현 완료

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/BlogServiceImpl.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/BlogServiceImpl.java
@@ -1,12 +1,19 @@
 package kr.pe.playdata.service.impl;
 
+import kr.pe.playdata.domain.Blog;
 import kr.pe.playdata.domain.Naver;
+import kr.pe.playdata.repository.NaverTemplateRepo;
 import kr.pe.playdata.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BlogServiceImpl implements BlogService {
-    public List<Naver> getTwoRecent(){
-        return null;
+    @Autowired
+    private NaverTemplateRepo repo;
+    public List<Naver> getTwoRecent(String tour){
+        return repo.getTwoRecent(tour);
     }
 }
