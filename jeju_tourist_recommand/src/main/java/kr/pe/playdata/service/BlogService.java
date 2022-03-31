27ed8a64c3ed3f0a5fa6bcf05f9b7fb38df5c1e0
dldydrhkd <sksda4614@naver.com>
2022-03-31 17:26:51 +0900

    Update BlogService.java
    Blog service 구현 완료

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/BlogService.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/BlogService.java
@@ -1,9 +1,10 @@
 package kr.pe.playdata.service;
 
+import kr.pe.playdata.domain.Blog;
 import kr.pe.playdata.domain.Naver;
 
 import java.util.List;
 
 public interface BlogService {
-    public List<Naver> getTwoRecent();
+    public List<Naver> getTwoRecent(String tour);
 }
