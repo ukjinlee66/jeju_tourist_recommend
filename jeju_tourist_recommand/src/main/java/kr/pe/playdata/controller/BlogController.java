4aee1366609262a7edfce1285b90dd53ad77e4b0
dldydrhkd <sksda4614@naver.com>
2022-03-31 17:25:30 +0900

    Blog Controller
    블로그 컨트롤러 생성

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/BlogController.java
@@ -0,0 +1,25 @@
+package kr.pe.playdata.controller;
+
+import kr.pe.playdata.domain.Blog;
+import kr.pe.playdata.domain.Naver;
+import kr.pe.playdata.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Blog")
+public class BlogController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/recentTwo")
+    public List<Naver> blogRecentTwo(@RequestParam String tour){
+        return blogService.getTwoRecent(tour);
+    }
+}
