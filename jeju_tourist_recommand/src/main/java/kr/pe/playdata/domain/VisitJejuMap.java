f24f517c96f2c63ec7a68ee9ade8757c19a48538
dldydrhkd <sksda4614@naver.com>
2022-04-02 20:18:06 +0900

    Create VisitJejuMap.java
    VisitJejuMap 새로 생성

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/domain/VisitJejuMap.java
@@ -0,0 +1,19 @@
+package kr.pe.playdata.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection="visitJeju")
+public class VisitJejuMap {
+    /*
+         map에서 쓰일 visitJeju VO
+     */
+
+    @Id
+    private String id;
+    private String tour;
+    private Location location;
+    private String img;
+}
