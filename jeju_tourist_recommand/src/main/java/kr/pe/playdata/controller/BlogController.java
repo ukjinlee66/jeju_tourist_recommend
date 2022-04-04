6c5f8baeb9a9ba1da939dc766a1dde5289f65c54
dldydrhkd <sksda4614@naver.com>
2022-04-04 22:44:00 +0900

    swagger 문서에 맞게 추가

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/BlogController.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/BlogController.java
@@ -1,5 +1,9 @@
 package kr.pe.playdata.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import kr.pe.playdata.domain.Naver;
 import kr.pe.playdata.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +24,16 @@ public class BlogController {
     @Autowired
     private BlogService blogService;
 
+    @ApiOperation(value = "최신 블로그 검색", notes = "특정 관광지와 관련된 가장 최신의 블로그 2개를 검색")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK !!"),
+            @ApiResponse(code = 404, message = "404 에러 발생, Not Found !"),
+            @ApiResponse(code = 500, message = "500 에러 발생, Internal Server Error !")
+    })
     @GetMapping("/recentTwo")
-    public List<Naver> blogRecentTwo(@RequestParam String source){
+    public List<Naver> blogRecentTwo(
+            @ApiParam(value = "관광지 이름", required=false, example = "우도(해양도립공원)")
+            @RequestParam String source){
         return blogService.getTwoRecent(source);
     }
 }
--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/KeywordRankController.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/controller/KeywordRankController.java
@@ -1,5 +1,8 @@
 package kr.pe.playdata.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import kr.pe.playdata.domain.InstaRank;
 import kr.pe.playdata.domain.SearchRank;
 import kr.pe.playdata.service.KeywordRankService;
@@ -20,11 +23,23 @@ public class KeywordRankController {
     @Autowired
     private KeywordRankService service;
 
+    @ApiOperation(value = "검색어 순위 조회", notes = "검색이 가장 많이 된 상위 5개의 검색어를 조회")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK !!"),
+            @ApiResponse(code = 404, message = "404 에러 발생, Not Found !"),
+            @ApiResponse(code = 500, message = "500 에러 발생, Internal Server Error !")
+    })
     @GetMapping("/searchKeyword")
     public List<SearchRank> getTopFiveSearchKeywords(){
         return service.getTopFiveSearchKeywords();          // 상위 5개의 검색어를 가져온다.
     }
 
+    @ApiOperation(value = "인스타 해쉬 태그 순위 조회", notes = "태그가 가장 많이 된 상위 5개 키워드 조회")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK !!"),
+            @ApiResponse(code = 404, message = "404 에러 발생, Not Found !"),
+            @ApiResponse(code = 500, message = "500 에러 발생, Internal Server Error !")
+    })
     @GetMapping("/instaKeyword")
     public List<InstaRank> getTopFiveInstaKeywords(){
         return service.getTopFiveInstaKeywords();           // 상위 5개의 인스타 키워드를 가져온다.
