a230819ae77b80c50b79e0f00a142fe5381cc431
dldydrhkd <sksda4614@naver.com>
2022-04-04 22:43:46 +0900

    swagger config 추가

---
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package kr.pe.playdata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2).ignoredParameterTypes(ApiIgnore.class)
+                .apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("kr.pe.playdata.controller"))
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("API Documentation")
+                .description("제주들려섬 API 문서 ")
+                .license("license : 제주들렸섬").licenseUrl("http://playdata.io/").build();
+    }
+}
