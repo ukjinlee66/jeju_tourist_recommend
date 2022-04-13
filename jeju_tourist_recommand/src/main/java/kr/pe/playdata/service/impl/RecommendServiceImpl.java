40eae8083e773273a49ef9f71cc11bba8f20490e
dldydrhkd <sksda4614@naver.com>
2022-04-13 21:22:50 +0900

    추천 시스템 변경
    flask post 요청 url 변경
    추천 서비스 몽고 변경

--- jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/RecommendServiceImpl.java
+++ jeju_tourist_recommand/src/main/java/kr/pe/playdata/service/impl/RecommendServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.pe.playdata.service.impl;
 
 import kr.pe.playdata.domain.VisitJeju;
+import kr.pe.playdata.domain.VisitJejuList;
 import kr.pe.playdata.repository.TouristAttractionMongoRepo;
 import kr.pe.playdata.service.RecommendService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,15 +38,15 @@ public class RecommendServiceImpl implements RecommendService {
     }
 
     @Async("asyncExecutor")
-    public CompletableFuture<List<VisitJeju>> relation(String sentence){
-        String url = "http://15.164.116.162:5000/recommend";
+    public CompletableFuture<List<VisitJejuList>> relation(String sentence){
+        String url = "http://15.164.116.162:5000/keyRecommend";
         MultiValueMap<String, String> param = new LinkedMultiValueMap<String, String>();
         param.add("sentence", sentence);
         String[] sb= restTemplate.postForObject(url, param, String[].class);
         List<String> li = Arrays.asList(sb);
-        List<VisitJeju> arr = new ArrayList<>();
+        List<VisitJejuList> arr = new ArrayList<>();
         for(int i=0; i<li.size(); i++){
-            arr.add(mongoRepo.findBySourceLike(li.get(i)));
+            arr.add(mongoRepo.findBySource(li.get(i)));
         }
         return CompletableFuture.completedFuture(arr);
     }
