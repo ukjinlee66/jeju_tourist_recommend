38aba45a4598a5b600fcb6dac75e3ceed0ee9622
dldydrhkd <sksda4614@naver.com>
2022-04-13 22:48:16 +0900

    Update server.py
    aws에서 사용할 수 있도록 flask ip 변경

--- recommend server/server.py
+++ recommend server/server.py
@@ -9,8 +9,8 @@ def recommend():
     return inference.inference(request.form.get('sentence'))
 
 @app.route('/keyRecommend', methods=['post'])
-def relation():
-    return inference.inference(request.form.get('sentence'))
+def keyRecommend():
+    return keyword_inference.keyword_inference(request.form.get('sentence'))
 
 if __name__ == "__main__":
     # host = "0.0.0.0" aws에서 부여받은 공유 ip로 외부에서 접속 가능
