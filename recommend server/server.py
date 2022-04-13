4d9423c4b0d1ad4db7ced217eb8e74f9d16ff544
Geonwoo <kkw2048@daum.net>
2022-04-13 19:22:24 +0900

    recommend server update

--- recommend server/server.py
+++ recommend server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 
 import inference
+import keyword_inference
 
 app = Flask(__name__)
 
@@ -11,6 +12,10 @@ def get():
 @app.route('/recommend', methods=['post'])
 def recommend():
     return inference.inference(request.form.get('sentence'))
+
+@app.route('/keyRecommend', methods=['post'])
+def keyRecommend():
+    return keyword_inference.keyword_inference(request.form.get('sentence'))
 
 if __name__ == "__main__":
     # host = "0.0.0.0" aws에서 부여받은 공유 ip로 외부에서 접속 가능
